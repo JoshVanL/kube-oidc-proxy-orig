683c2004049506e6195f7f2a5681284ef8b995e1
JoshVanL <vleeuwenjoshua@gmail.com>
2019-03-18 16:52:18 +0000

    Adds basic e2e tests for tokens and rbac
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- pkg/e2e/issuer/issuer.go
+++ pkg/e2e/issuer/issuer.go
@@ -2,9 +2,10 @@
 package issuer
 
 import (
+	"crypto/rsa"
+	"encoding/base64"
 	"fmt"
 	"net/http"
-	"testing"
 	"time"
 
 	"github.com/jetstack/kube-oidc-proxy/pkg/utils"
@@ -16,13 +17,12 @@ type Issuer struct {
 	listenPort        string
 	certPath, keyPath string
 
-	t *testing.T
+	sk *rsa.PrivateKey
 }
 
-func New(t *testing.T, tlsDir string) *Issuer {
+func New(tlsDir string) *Issuer {
 	return &Issuer{
 		tlsDir: tlsDir,
-		t:      t,
 	}
 }
 
@@ -33,19 +33,20 @@ func (i *Issuer) Run() error {
 	}
 	i.listenPort = listenPort
 
-	certPath, keyPath, err := utils.NewTLSSelfSignedCertKey(i.tlsDir, "oidc-issuer")
+	certPath, keyPath, sk, _, err := utils.NewTLSSelfSignedCertKey(i.tlsDir, "oidc-issuer")
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create issuer key pair: %s", err)
 	}
 	i.certPath = certPath
 	i.keyPath = keyPath
+	i.sk = sk
 
 	serveAddr := fmt.Sprintf("127.0.0.1:%s", i.listenPort)
 
 	go func() {
 		err = http.ListenAndServeTLS(serveAddr, i.certPath, i.keyPath, i)
 		if err != nil {
-			i.t.Errorf("failed to server secure tls: %s", err)
+			klog.Errorf("failed to server secure tls: %s", err)
 		}
 	}()
 
@@ -64,8 +65,14 @@ func (i *Issuer) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		rw.Header().Set("Content-Type", "application/json")
 		rw.Write(i.wellKnownResponse())
 
+	case "/certs":
+		rw.Header().Set("Content-Type", "application/json")
+
+		discCerts := i.CertsDisc()
+		rw.Write(discCerts)
+
 	default:
-		i.t.Errorf("unexpected URL request: %s", r.URL)
+		klog.Errorf("unexpected URL request: %s", r.URL)
 	}
 }
 
@@ -75,6 +82,10 @@ func (i *Issuer) CertPath() string {
 
 func (i *Issuer) KeyPath() string {
 	return i.keyPath
+}
+
+func (i *Issuer) Key() *rsa.PrivateKey {
+	return i.sk
 }
 
 func (i *Issuer) Port() string {
@@ -102,6 +113,7 @@ func (i *Issuer) wellKnownResponse() []byte {
  "claims_supported": [
   "email",
 	"e2e-username-claim",
+	"e2e-groups-claim",
   "sub"
  ],
  "code_challenge_methods_supported": [
@@ -109,4 +121,21 @@ func (i *Issuer) wellKnownResponse() []byte {
   "S256"
  ]
 }`, i.listenPort, i.listenPort))
+}
+
+func (i *Issuer) CertsDisc() []byte {
+	n := base64.RawURLEncoding.EncodeToString(i.sk.N.Bytes())
+
+	return []byte(fmt.Sprintf(`{
+	  "keys": [
+	    {
+	      "kid": "0905d6f9cd9b0f1f852e8b207e8f673abca4bf75",
+	      "e": "AQAB",
+	      "kty": "RSA",
+	      "alg": "RS256",
+	      "n": "%s",
+	      "use": "sig"
+	    }
+	  ]
+	}`, n))
 }
